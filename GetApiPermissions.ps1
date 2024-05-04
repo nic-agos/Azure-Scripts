494f6a3af722401ee715a9997247fdf0efdf87fd
Niccolò Agostinelli <nic-agos@users.noreply.github.com>
2024-05-04 17:09:51 +0200

    Some improvement in API Permissions file

--- GetApiPermissions.ps1
+++ GetApiPermissions.ps1
@@ -1,10 +1,27 @@
+# Set the header for the CSV file
+$header = "Application Display Name;Application (client) ID;Creation DateTime;Object ID;API Display Name;API ID;Permission; Permission Description"
+
+# Set the result sets as empty
+$resultSet = ""
+
 $adApplications = Get-AzADApplication
 
 foreach($adApplication in $adApplications){
-    
-    $appId = $adApplication.Id
 
-    $app = Get-AzADApplication -ObjectId $appId
+    $appCreatedDateTime = $adApplication.CreatedDateTime
+
+    $currentTuple = ""
+    
+    # Get the Application (client) Display Name
+    $appDisplayName = $adApplication.DisplayName
+
+    # Get the Application (client) ID
+    $appId = $adApplication.AppId
+
+    # Get the object ID
+    $objectId = $adApplication.Id
+
+    $app = Get-AzADApplication -ObjectId $objectId
 
     $requiredResourceAccess = $app.requiredResourceAccess
 
@@ -12,32 +29,89 @@ foreach($adApplication in $adApplications){
 
         foreach($resource in $resourceAccess){
 
-            $resourceAppId = $resource.ResourceAppId
+            # Get the API ID
+            $resourceApiId = $resource.ResourceAppId
+            
+            # Get the permission ID
             $permissionId = $resource.ResourceAccess.Id
+
+            #Get the permission type
             $permissionType = $resource.ResourceAccess.Type
 
-            $servicePrincipal = Get-AzADServicePrincipal -ApplicationId $resourceAppId
+            # Get the API object
+            $api = Get-AzADServicePrincipal -ApplicationId $resourceApiId
             
-            $servicePrincipalDisplayName = $servicePrincipal.AppDisplayName
+            # Get the API Display Name
+            $apiDisplayName = $api.AppDisplayName
             
-            $appRoles = $servicePrincipal.AppRole 
-            $oauth2PermissionScope = $servicePrincipal.Oauth2PermissionScope
+            # Get the API App Roles
+            $appRoles = $api.AppRole 
+
+            # Get the API Oauth2 Permissions
+            $oauth2PermissionScope = $api.Oauth2PermissionScope
 
             if($permissionType -eq "Scope"){
-
+                
+                # Get the permission object
                 $permissionInScope = $oauth2PermissionScope | Where-Object {$_.id -eq $permissionId}
+
+                # Get the permission name
                 $permissionInScopeValue = $permissionInScope.Value
-                echo $permissionInScopeValue
+
+                # Get the permission description
+                $permissionInScopeAdminConsentDescription = $permissionInScope.AdminConsentDescription
+
+                $currentTuple = "`n" + 
+                    $appDisplayName + ";" + 
+                    $appId + ";" +
+                    $appCreatedDateTime + ";" +
+                    $objectId + ";" +
+                    $apiDisplayName + ";" +
+                    $resourceApiId + ";" +
+                    $permissionInScopeValue + ";" +
+                    $permissionInScopeAdminConsentDescription
+                
             }
 
             if ($permissionType -eq "Role"){
+
+                # Get the permission object
                 $permissionInRole = $appRoles | Where-Object {$_.id -eq $permissionId}
-                $permissionInRoleValue = $permissionInScope.Value
-                echo $permissionInRoleValue
+
+                # Get the permission name
+                $permissionInRoleValue = $permissionInRole.Value
+
+                # Get the permission description
+                $permissionInRoleAdminConsentDescription = $permissionInRole.AdminConsentDescription
+
+                $currentTuple = "`n" + 
+                    $appDisplayName + ";" + 
+                    $appId + ";" +
+                    $appCreatedDateTime + ";" +
+                    $objectId + ";" +
+                    $apiDisplayName + ";" +
+                    $resourceApiId + ";" +
+                    $permissionInRoleValue + ";" +
+                    $permissionInRoleAdminConsentDescription
+
             }
         
+            $resultSet = $resultSet + $currentTuple
+
         }
         
     }
     
 }
+
+# Get the current date
+$date = Get-Date -Format "yyyy-MM-dd"
+
+# Concat the header row with the result set rows
+$result = ($header + $resultSet)
+
+# Set the output file name and path
+$path = "./API Permissions_" + $date + ".csv"
+
+# Write the result in the output file
+$result | Out-File -FilePath $path
